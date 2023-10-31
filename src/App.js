2d9cdbcb82b283b02b63e04214b77a3a95dced5c
Sandhya Vinod <back2sandhya@gmail.com>
2023-11-01 01:32:03 +0530

    ClickMe

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Calendar from './Components/Calendar';
 import ToDo from './Components/ToDo';
 import Hello from './Components/Hello';
+import ClickMe from './Components/ClickMe';
 
 function App() {
   return (
     <div>
      <Hello/>
-      <Calendar/>
+    <Calendar/>
+    <ClickMe/>
       <ToDo/>
     </div>
   );
---
+++ src/Components/ClickMe.js
@@ -0,0 +1,24 @@
+import React from 'react';
+class ClickMe extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'Initial State'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      // Change code below this line
+  this.setState({name:'Have a nice day!'})
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click Me</button>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
+  export default ClickMe;
