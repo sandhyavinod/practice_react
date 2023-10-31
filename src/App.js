888ac08b92c794715b86cb7d7927f0c9821e1f2b
Sandhya Vinod <back2sandhya@gmail.com>
2023-11-01 00:10:35 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Calendar from './Components/Calendar';
 import ToDo from './Components/ToDo';
+import Hello from './Components/Hello';
 
 function App() {
   return (
     <div>
+     <Hello/>
       <Calendar/>
       <ToDo/>
     </div>
---
+++ src/Components/Hello.js
@@ -0,0 +1,29 @@
+import React from 'react';
+class Hello extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+    render() {
+      return (
+          <div><Welcome name="sandhya"/></div>
+      );
+    }
+  };
+
+  class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+    render() {
+      return (
+          <div>
+
+            <p>Hello,<strong>{this.props.name}!</strong></p>
+
+          </div>
+      );
+    }
+  };
+  export default Hello;
